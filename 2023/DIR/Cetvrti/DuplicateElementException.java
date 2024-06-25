c8efdeae205fce0276acbeeced64ac64221aa711
iLeon25 <163464927+iLeon25@users.noreply.github.com>
2024-06-25 21:33:47 +0200

    Create DuplicateElementException.java

---
+++ 2023/DIR/Cetvrti/DuplicateElementException.java
@@ -0,0 +1,11 @@
+package hr.fer.oop;
+
+
+@SuppressWarnings("serial")
+public class DuplicateElementException extends RuntimeException{
+
+	public DuplicateElementException(String message) {
+		super(message);
+	}
+	
+}
